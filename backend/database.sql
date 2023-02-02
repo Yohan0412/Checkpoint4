440d43d98ea21ae3e32b647fd2b32f21b9fa8495
Yohan0412 <Yohan0412>
2023-02-02 11:49:44 +0100

    ajout fichier DB

--- backend/database.sql
+++ backend/database.sql
@@ -4,3 +4,21 @@ CREATE TABLE item (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+
+mysql> CREATE TABLE `planete` (
+     `id` INT NOT NULL AUTO_INCREMENT,
+     `nom` VARCHAR(100) NOT NULL,
+     `distance` INT NOT NULL,
+     `temp` INT NOT NULL,
+     `prix` INT NOT NULL,
+     `image` VARCHAR(255) NOT NULL,
+     PRIMARY KEY (`id`)
+     );
+
+
+mysql> CREATE TABLE `transport` (
+     `id` INT NOT NULL AUTO_INCREMENT,
+     `nom` VARCHAR(50),
+     `image` VARCHAR(255),
+     PRIMARY KEY (`id`)
+     );
